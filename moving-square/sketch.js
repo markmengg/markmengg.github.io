a95355491f4da4d8115daf200bdfcc3e9ec1199e
markmengg <markmengg@gmail.com>
2024-09-19 14:05:08 -0600

    added moving square

---
+++ moving-square/sketch.js
@@ -0,0 +1,56 @@
+// Square Moving Around Screen
+// Sept 19, 2024
+
+
+
+let x = 0;
+let y = 0;
+let speed = 5;
+let state = "right";
+let squareSize = 50;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  moveSquare();
+  displaySquare();
+}
+
+
+function moveSquare() {
+  if (state === "right"){
+    x += speed
+    if (x >= width - squareSize){
+      state = "down";
+    }
+  }
+  
+  if (state === "down"){
+    y += speed;
+    if (y >= height - squareSize){
+      state = "left";
+    }
+  }
+  
+  if (state === "left"){
+  x -= speed
+    if (x <= 0){
+      state = "up";
+    }
+  }
+  
+  if (state === "up"){
+    y -= speed;
+    if (y <= 0){
+      state = "right";
+    }
+  }
+}
+
+function displaySquare() {
+  fill("beige")
+  square(x, y, squareSize)
+}
