3938181d8f63ac5d21da0d12ddaa6002e8bcf33f
markmengg <markmengg@gmail.com>
2024-09-25 14:16:44 -0600

    played around a little

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -10,8 +10,8 @@
 // Stating Variables
 let radius = 40;
 let circleSize = radius;
-let sizeChange = 3;
-let shapeType = 'circle'; // Default shape
+const sizeChange = 3;
+let shapeType = "circle"; // Default shape
 let backgroundColor = 150;
 let colorChange = 20;
 let brush;
@@ -27,10 +27,11 @@ function setup() {
   // Makes it so that the size of the window is adjusted to / filled by the canvas
   createCanvas(windowWidth, windowHeight);
   background(backgroundColor);
+  frameRate(60);
 }
 
 function draw() {
-  cursor(CROSS, mouseX, mouseY)
+  cursor(CROSS, mouseX, mouseY);
 }
 
 
@@ -51,50 +52,53 @@ function drawShape() {
   noStroke();
   
   // Draw the selected shape
-  if (shapeType === 'circle') {
+  if (shapeType === "circle") {
     circle(mouseX, mouseY, circleSize);
-  } else if (shapeType === 'square') {
+  }
+  else if (shapeType === "square") {
     square(mouseX - circleSize / 2, mouseY - circleSize / 2, circleSize);
-  } else if (shapeType === 'triangle') {
+  }
+  else if (shapeType === "triangle") {
     triangle(mouseX, mouseY - circleSize / 2, mouseX - circleSize / 2, mouseY + circleSize / 2, mouseX + circleSize / 2, mouseY + circleSize / 2);
   }
 }
 
 // Increases the size of the shape when the user scrolls the mouse wheel. (Scroll up = increase size, scroll down = decrease size)
-
 function mouseWheel(event) {
   let direction = event.delta;
   
   if (direction > 0 && circleSize > 5) {
     circleSize -= sizeChange;
-  } else {
+  }
+  else {
     circleSize += sizeChange;
   }
 }
 
 // When user presses any key on the keyboard, it changes resets circleSize and shape to circle. If they press S, it turns into a square. If they press T, it turns into a triangle
-
 function keyPressed() {
-  if (key === '1') {
-    shapeType = 'circle'; // Change to circle
+  if (key === "1") {
+    shapeType = "circle"; // Change to circle
   } 
-  else if (key === '2') {
-    shapeType = 'square'; // Change to square
+  else if (key === "2") {
+    shapeType = "square"; // Change to square
   }
-  else if (key === '3') {
-    shapeType = 'triangle'; // Default to triangle
+  else if (key === "3") {
+    shapeType = "triangle"; // Default to triangle
   }
-  else if (key === 'q') {
+  else if (key === "e") {
     circleSize = radius; // Reset size
-    clear()
-    setup()
   }
-  else if (key === 'o') {
+  else if (key === "q") {
+    clear();
+    setup();
+  }
+  else if (key === "o") {
     backgroundColor = backgroundColor + colorChange;
-    setup()
+    setup();
   }
-  else if (key === 'p') {
+  else if (key === "p") {
     backgroundColor = backgroundColor - colorChange;
-    setup()
+    setup();
   }
 }
