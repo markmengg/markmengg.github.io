86c3188708f0df4083a8c1ff3a0e81d0b49c280c
markmengg <markmengg@gmail.com>
2024-10-08 14:00:29 -0600

    Created a background for my snake game

---
+++ array-object/sketch.js
@@ -0,0 +1,19 @@
+// Bloons TD6 Ripoff? some sort of tower/base defense game with pathways or snake
+// Mark Meng
+// a star algorithm
+// October 8th, 2024
+
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let bg;
+
+function setup() {
+  bg = loadImage("snakeBG.jpg");
+  createCanvas(548, 483);
+}
+
+function draw() {
+  background(bg);
+}
+
