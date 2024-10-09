82c6641b9020c6d1ac242981087b194c129b0786
markmengg <markmengg@gmail.com>
2024-10-09 14:14:04 -0600

    set up an object notation for my canvas to define a grid where the snake(player) will be able to move. Left off confused as the code seems to use the grid lines as the picture instead of using the grid lines overtop the picture. Considering ditching the grid and making my own?

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -7,13 +7,32 @@
 // - describe what you did to take this project "above and beyond"
 
 let bg;
+let width = 548;
+let height = 483;
+
 
 function setup() {
+  createCanvas(width, height);
   bg = loadImage("snakeBG.jpg");
-  createCanvas(548, 483);
 }
 
 function draw() {
   background(bg);
+  drawGrid();   
 }
 
+function drawGrid() {
+  for (let y = 0; y < theGrid.yAmount; y++) {
+    for (let x = 0; x < theGrid.xAmount; x++){     
+      noStroke();
+      rect(x*theGrid.xSize, y*theGrid.ySize, theGrid.xSize, theGrid.ySize);
+    }
+  }
+}
+
+let theGrid = {
+  xAmount: 17,
+  yAmount: 15,
+  xSize: width/17,
+  ySize: height/15,
+};
