a6dab2d616ad58f0d03f0d3d252f573a38c9ce48
markmengg <markmengg@gmail.com>
2024-10-13 10:56:32 -0600

    Made a snake, let it move based on user controls, adjusted speed of snake so that it wouldn't be 60fps, added apples that grow the snake upon collisions.
    I am hoping to add snake/wall collisions, maybe mines that would pop/splice a block out of the snake upon collision, starting screen and an ending screen, and fix further errors (ie: if two keys are pressed opposite of each other then it will go backwards.

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -6,18 +6,37 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+
+let direction = {x: 1, y: 0}
 let width = 700;
 let height = 600;
-let snake = [];
+let snake = [{x: 8, y: 7}];
 let apples = [];
+let theGrid = {
+  xAmount: 17,
+  yAmount: 15,
+  xSize: width/17,
+  ySize: height/15,
+};
+let moveInterval = 150;
+let lastMoveTime = 0
+
 
 function setup() {
   createCanvas(width, height);
+  spawnApple();
 }
 
 function draw() {
   background(255);
-  drawGrid();   
+  drawGrid(); 
+  if (millis() - lastMoveTime > moveInterval) { // Check if it's time to move the snake
+    moveSnake();
+    lastMoveTime = millis(); // Update last move time
+  }
+  checkCollisions();
+  drawSnake(); 
+  drawApples();
 }
 
 function drawGrid() {
@@ -37,9 +56,58 @@ function drawGrid() {
   }
 }
 
-let theGrid = {
-  xAmount: 17,
-  yAmount: 15,
-  xSize: width/17,
-  ySize: height/15,
-};
+function drawSnake() {
+  fill(130, 159, 255);
+  for (let i = 0; i < snake.length; i++) {
+    rect(snake[i].x * theGrid.xSize, snake[i].y * theGrid.ySize, theGrid.xSize, theGrid.ySize);
+  }
+}
+
+function moveSnake() {
+  let newHead = {
+    x: snake[0].x + direction.x,
+    y: snake[0].y + direction.y,
+  }
+  snake.unshift(newHead);
+  snake.pop();
+}
+
+
+function keyPressed() {
+  if (keyCode === UP_ARROW || key === 'W') {
+    direction = {x: 0, y: -1};
+  } else if (keyCode === DOWN_ARROW || key === 'S') {
+    direction = {x: 0, y: 1};
+  } else if (keyCode === LEFT_ARROW || key === 'A') {
+    direction = {x: -1, y: 0};
+  } else if (keyCode === RIGHT_ARROW || key === 'D') {
+    direction = {x: 1, y: 0};
+  }
+}
+
+function drawApples() {
+  fill(255, 0, 0);
+  for (let i = 0; i < apples.length; i++) {
+    rect(apples[i].x * theGrid.xSize, apples[i].y * theGrid.ySize, theGrid.xSize, theGrid.ySize);
+  }
+}
+
+function spawnApple() {
+  let apple = {
+    x: floor(random(0, theGrid.xAmount)),
+    y: floor(random(0, theGrid.yAmount)),
+  };
+  apples.push(apple);
+}
+
+
+function checkCollisions() {
+  let head = snake[0];
+  for (let i = 0; i < apples.length; i++) {
+    if (head.x === apples[i].x && head.y === apples[i].y) {
+      apples.splice(i, 1);
+      spawnApple();
+      snake.push({});
+    }
+  }
+}
